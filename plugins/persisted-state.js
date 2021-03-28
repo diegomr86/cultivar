9c091e2d7f92225520c2dec9d0189f2787979e74
Diego M. Rodrigues <diegomr86@gmail.com>
2021-03-28 14:56:13 -0300

    Corrige bug de cookie no server render

--- plugins/persisted-state.js
+++ plugins/persisted-state.js
@@ -4,12 +4,16 @@ import cookie from 'cookie'
 
 export default ({ store, req }) => {
   createPersistedState({
-    // paths: [...],
+    paths: ['region'],
     storage: {
       getItem: (key) => {
         // See https://nuxtjs.org/guide/plugins/#using-process-flags
         if (process.server) {
-          const parsedCookies = cookie.parse(req.headers.cookie)
+          let headerCookie = req.headers.cookie
+          if (typeof headerCookie !== 'string') {
+            headerCookie = ''
+          }
+          const parsedCookies = cookie.parse(headerCookie)
           return parsedCookies[key]
         } else {
           return Cookies.get(key)
